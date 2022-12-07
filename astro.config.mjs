ca53afd7dc99d84c64449339cc89d5820843ee18
Angus Findlay <angus.findlay@icloud.com>
2022-12-07 18:09:30 +0000

    added vercel ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import elm from "astro-integration-elm";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [elm()],
+  output: "server",
+  adapter: vercel(),
 });
